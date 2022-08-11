c9828e1002ca7e739b5f75beda0be221e95f92c5
Siddharth yadav <93334655+yadav-siddharth@users.noreply.github.com>
2022-08-11 09:37:11 +0530

    Update stock_buy_sell.py

--- SDE_Sheet/stock_buy_sell.py
+++ SDE_Sheet/stock_buy_sell.py
@@ -1,13 +1,14 @@
 def stock_buy(prices):
-          left,right = 0,1
-          maxPro = 0
+          left,right = 0,1                                         # creating two pointer left, right for numbers of days comaparison
+          maxPro = 0                                               # initiaizing max_profit to 0
 
-          while right < len(prices):
-                    if prices[left] < prices[right]:
+          while right < len(prices):                               # looping till the length of list 
+                    if prices[left] < prices[right]:               # comparing left and right day price if right is greater then substract with left to calculate the                                                                             profit
+                              
                               profit = prices[right] - prices[left]
-                              maxPro = max(maxPro,profit)
+                              maxPro = max(maxPro,profit)                        #finding the maxprofit from profit and maxprofit stored 
                     else:
-                              left = right
+                              left = right                                       # otherwise if left is greater than right then bring the left pointer to right and                                                                                            increment right by 1
                     right += 1
 
           print(maxPro)                              
